e8daab886d7e59c47b384d5096fce1b5e5845ccd
RyanColton <ryan.m.colton@gmail.com>
2017-01-17 12:03:31 -0700

    Finishing the project

--- controller.js
+++ controller.js
@@ -1,3 +1,19 @@
 angular.module('myApp').controller('mainCtrl', function($scope, groceryService){
 
+  $scope.groceries = groceryService.getGroceries();
+
+  $scope.cart = [];
+
+  $scope.addItemToCart = function(grocery){
+    $scope.cart.push(grocery);
+  }
+
+  $scope.total = function(){
+    var total = 0;
+    for(var i=0; i < $scope.cart.length; i++){
+      total += $scope.cart[i].price;
+    }
+    return total;
+  }
+
 })
--- services.js
+++ services.js
@@ -1,3 +1,23 @@
 angular.module('myApp').service('groceryService', function(){
+ var groceryItems = [
+   {
+     name: 'Butter',
+     price: 2.50
+   },
+   {
+     name: 'Carrots',
+     price: .35
+   },
+   {
+     name: 'Ground Beef',
+     price: 2.99
+   },
+   {
+     name: 'Lucky Charms',
+     price: 2.99
+   }];
 
+   this.getGroceries = function(){
+     return groceryItems;
+   }
 })
